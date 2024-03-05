e2dced00c91e7c4041f6abfefbf8e375802ea11c
Vinh Nguyen <ntanvinh@tma.com.vn>
2024-03-06 00:30:15 +0700

    Add Porter Sans Block font and update button styles

--- src/pages/RootLayout/RootLayout.tsx
+++ src/pages/RootLayout/RootLayout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Logo from "../../assets/logo.svg";
 
 import "./RootLayout.css";
+import { Button } from "antd";
 
 const RootLayout: React.FC = () => {
   return (
@@ -46,49 +47,87 @@ const RootLayout: React.FC = () => {
             marginRight: 200,
           }}
         >
-          <button
+          <Button
             style={{
               width: 122,
-              height: 50,
+              height: 48,
               backgroundColor: "#ffffff",
-              borderRadius: 10,
-              border: "none",
-              fontFamily: "Zen Dots",
+              borderRadius: 8,
+              border: "1.5px solid #272833",
+              fontFamily: "Porter Sans Block",
               fontSize: 20,
+              fontWeight: 400,
               marginRight: 16,
+              boxShadow: "0px 4px 4px 0px #000000",
             }}
           >
-            Start
-          </button>
-          <button
+            DATA
+          </Button>
+          <Button
             style={{
               width: 122,
-              height: 50,
+              height: 48,
               backgroundColor: "#ffffff",
-              borderRadius: 10,
-              border: "none",
-              fontFamily: "Zen Dots",
+              borderRadius: 8,
+              border: "1.5px solid #272833",
+              fontFamily: "Porter Sans Block",
               fontSize: 20,
+              fontWeight: 400,
               marginRight: 16,
+              boxShadow: "0px 4px 4px 0px #000000",
             }}
           >
-            Settings
-          </button>
-          <button
+            AI
+          </Button>
+          <Button
             style={{
               width: 122,
-              height: 50,
+              height: 48,
               backgroundColor: "#ffffff",
-              borderRadius: 10,
-              border: "none",
-              fontFamily: "Zen Dots",
+              borderRadius: 8,
+              fontFamily: "Porter Sans Block",
               fontSize: 20,
+              fontWeight: 400,
+              border: "1.5px solid #272833",
+              boxShadow: "0px 4px 4px 0px #000000",
             }}
           >
-            Exit
-          </button>
+            BOT
+          </Button>
         </div>
       </div>
+      <div
+        style={{
+          width: '65vw',
+          height: '70vh',
+          backgroundColor: "#ffffff",
+          borderRadius: 24,
+          border: "2px solid #272833",
+          margin: "auto",
+          marginTop: 16,
+          marginBottom: 16,
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+      </div>
+      <Button
+        style={{
+          width: 177,
+          height: 60,
+          borderRadius: 8,
+          border: "1px solid #ffffff",
+          boxShadow: "0px 4px 4px 0px #000000",
+          fontFamily: "Porter Sans Block",
+          fontSize: 20,
+          alignSelf: "center",
+          marginTop: 16,
+          backgroundColor: "#272833",
+          color: "#ffffff",
+        }}
+      >
+        PLAY
+      </Button>
     </div>
   );
 };
